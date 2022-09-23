4e6d9017fb336e343ba514d0fe8801c031561af7
graziosi <luizgraziosi@hotmail.com>
2022-09-23 11:37:42 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Navegation from './components/Navegation'
+import './main.css'
+
+function App() {
+
+  return (
+    <div className="max-w-[1360px] mx-auto">
+      <header className="">
+        <Navegation/>
+      </header>
+
+      {/*<main className="my-20">
+        <div className=" items-center justify-center text-7xl">
+          <p className="text-white">Great Coffee</p>
+          <p className="text-white">Great Code</p>
+          <img src="./rocket-coffee.png" alt="" />
+        </div>
+      </main>
+      <footer className="">
+        <p className="text-white">Copy</p>
+  </footer>*/}
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.jsx',
+    './index.html',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Manrope', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
